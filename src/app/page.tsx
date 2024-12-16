cfb2ca9462eb9ff4dac05ab7116632f654d5668b
unknown <omaralghish@gmail.com>
2024-12-16 17:29:30 +0200

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import ThemeToggle from "@/components/themeToggle/ThemeToggle";
 import { Header } from "@/components";
 
 export default function Home() {
-  const [_, setIsOpen] = useState(false);
-
+  const [isOpen, setIsOpen] = useState(false);
+console.log(isOpen)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] h-[300vh]">
       <Button
