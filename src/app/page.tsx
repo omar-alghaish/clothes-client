a3e5fa1006464369105c6a09d1f6e068deb7433f
unknown <omaralghish@gmail.com>
2024-12-16 17:23:49 +0200

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { Button, Modal, Drawer } from "@/components/common";
+import { Button } from "@/components/common";
 import "../styles/styles.css";
 import { useState } from "react";
 import ThemeToggle from "@/components/themeToggle/ThemeToggle";
 import { Header } from "@/components";
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [_, setIsOpen] = useState(false);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] h-[300vh]">
